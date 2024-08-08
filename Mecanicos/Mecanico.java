c40b6caad3af6e92a4546d43f63111eb48ca2922
chiza <luisdanielav05@gmail.com>
2024-08-08 14:54:16 -0600

    Clase Mecanico y Clase Vehiculo
    Se crean las clases mecanico y vehiculo con sus atributos y metodos

--- Mecanicos/Mecanico.java
+++ Mecanicos/Mecanico.java
@@ -9,5 +9,39 @@ package Mecanicos;
  * @author Luisk
  */
 public class Mecanico {
+    private int id;
+    private String nombre;
+    private String especialidad;
+
+    public Mecanico(int id, String nombre, String especialidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.especialidad = especialidad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Mecanico{" + "id=" + id + ", nombre=" + nombre + ", especialidad=" + especialidad + '}';
+    }
     
 }
--- Vehiculos/Vehiculo.java
+++ Vehiculos/Vehiculo.java
@@ -9,5 +9,49 @@ package Vehiculos;
  * @author Luisk
  */
 public class Vehiculo {
-    
+    private String matricula;
+    private String modelo;
+    private String marca;
+    private int anioFabricacion;
+
+    public Vehiculo(String matricula, String modelo, String marca, int anioFabricacion) {
+        this.matricula = matricula;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.anioFabricacion = anioFabricacion;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public int getAnioFabricacion() {
+        return anioFabricacion;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Vehiculo [Matrícula=" + matricula + ", Modelo=" + modelo + ", Marca=" + marca + ", Año de Fabricación=" + anioFabricacion + "]";
+    }
 }
